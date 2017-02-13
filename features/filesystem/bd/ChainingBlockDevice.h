geky/mbed

bd: Added utility block device classes
- ChainingBlockDevice
- SlicingBlockDevice

features/filesystem/bd/ChainingBlockDevice.h
@@ -1,0 +1,159 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2006-2012 ARM Limited
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#ifndef MBED_CHAINING_BLOCK_DEVICE_H
+#define MBED_CHAINING_BLOCK_DEVICE_H
+
+#include "BlockDevice.h"
+#include "mbed.h"
+
+
+/** Block device for chaining multiple block devices
+ *  with the similar block sizes at sequential addresses
+ *
+ *  @code
+ *  #include "mbed.h"
+ *  #include "HeapBlockDevice.h"
+ *  #include "ChainingBlockDevice.h"
+ *
+ *  // Create two smaller block devices with
+ *  // 64 and 32 blocks of size 512 bytes
+ *  HeapBlockDevice mem1(64*512, 512);
+ *  HeapBlockDevice mem2(32*512, 512);
+ *
+ *  // Create a block device backed by mem1 and mem2
+ *  // contains 96 blocks of size 512 bytes
+ *  BlockDevice *bds[] = {&mem1, &mem2};
+ *  ChainingBlockDevice chainmem(bds);
+ */
+class ChainingBlockDevice : public BlockDevice {
+public:
+    /** Lifetime of the memory block device
+     *
+     *  @param bds      Array of block devices to chain with sequential block addresses
+     *  @param count    Number of block devices to chain
+     *  @note All block devices must have the same block size
+     */
+    ChainingBlockDevice(BlockDevice **bds, size_t bd_count);
+
+    /** Lifetime of the memory block device
+     *
+     *  @param bds      Array of block devices to chain with sequential block addresses
+     *  @note All block devices must have the same block size
+     */
+    template <size_t SIZE>
+    ChainingBlockDevice(BlockDevice *(&bds)[SIZE])
+        : _bds(bds), _bd_count(sizeof(bds) / sizeof(bds[0]))
+        , _read_size(0), _program_size(0), _erase_size(0), _size(0) {
+    }
+
+    /** Lifetime of the memory block device
+     *
+     *  @param bds      Array of block devices to chain with sequential block addresses
+     *  @note All block devices must have the same block size
+     */
+    virtual ~ChainingBlockDevice() {}
+
+    /** Initialize a block device
+     *
+     *  @return         0 on success or a negative error code on failure
+     */
+    virtual bd_error_t init();
+
+    /** Deinitialize a block device
+     *
+     *  @return         0 on success or a negative error code on failure
+     */
+    virtual bd_error_t deinit();
+
+    /** Read blocks from a block device
+     *
+     *  @param buffer   Buffer to write blocks to
+     *  @param addr     Address of block to begin reading from
+     *  @param size     Size to read in bytes, must be a multiple of read block size
+     *  @return         0 on success, negative error code on failure
+     */
+    virtual bd_error_t read(void *buffer, bd_addr_t addr, bd_size_t size);
+
+    /** Program blocks to a block device
+     *
+     *  The blocks must have been erased prior to being programmed
+     *
+     *  @param buffer   Buffer of data to write to blocks
+     *  @param addr     Address of block to begin writing to
+     *  @param size     Size to write in bytes, must be a multiple of program block size
+     *  @return         0 on success, negative error code on failure
+     */
+    virtual bd_error_t program(const void *buffer, bd_addr_t addr, bd_size_t size);
+
+    /** Erase blocks on a block device
+     *
+     *  The state of an erased block is undefined until it has been programmed
+     *
+     *  @param addr     Address of block to begin erasing
+     *  @param size     Size to erase in bytes, must be a multiple of erase block size
+     *  @return         0 on success, negative error code on failure
+     */
+    virtual bd_error_t erase(bd_addr_t addr, bd_size_t size);
+
+    /** Sync an underlying block device
+     *
+     *  @return         0 on success or a negative error code on failure
+     */
+    virtual bd_error_t sync();
+
+    /** Get the size of a readable block
+     *
+     *  @return         Size of a readable block in bytes
+     */
+    virtual bd_size_t read_size();
+
+    /** Get the size of a programable block
+     *
+     *  @return         Size of a programable block in bytes
+     *  @note Must be a multiple of the read size
+     */
+    virtual bd_size_t program_size();
+
+    /** Get the size of a eraseable block
+     *
+     *  @return         Size of a eraseable block in bytes
+     *  @note Must be a multiple of the program size
+     */
+    virtual bd_size_t erase_size();
+
+    /** Get the total size of the underlying device
+     *
+     *  @return         Size of the underlying device in bytes
+     */
+    virtual bd_size_t size();
+
+protected:
+    BlockDevice **_bds;
+    size_t _bd_count;
+    bd_size_t _read_size;
+    bd_size_t _program_size;
+    bd_size_t _erase_size;
+    bd_size_t _size;
+};
+
+
+#endif

features/filesystem/bd/SlicingBlockDevice.h
@@ -1,0 +1,165 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2006-2012 ARM Limited
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#ifndef MBED_SLICING_BLOCK_DEVICE_H
+#define MBED_SLICING_BLOCK_DEVICE_H
+
+#include "BlockDevice.h"
+#include "mbed.h"
+
+
+/** Block device for mapping to a slice of another block device
+ *
+ *  @code
+ *  #include "mbed.h"
+ *  #include "HeapBlockDevice.h"
+ *  #include "SlicingBlockDevice.h"
+ *
+ *  // Create a block device with 64 blocks of size 512
+ *  HeapBlockDevice mem(64*512, 512);
+ *
+ *  // Create a block device that maps to the first 32 blocks
+ *  SlicingBlockDevice slice1(&mem, 0*512, 32*512);
+ *
+ *  // Create a block device that maps to the last 32 blocks
+ *  SlicingBlockDevice slice2(&mem, 32*512);
+ *
+ *  // Create a block device that maps to the middle 32 blocks
+ *  SlicingBlockDevice slice3(&mem, 16*512, -16*512);
+ */
+class SlicingBlockDevice : public BlockDevice {
+public:
+    /** Lifetime of the memory block device
+     *
+     *  @param bd       Block device to back the SlicingBlockDevice
+     *  @param start    Start block address to map to block 0, negative addresses
+     *                  are calculated from the end of the underlying block device
+     *  @note This is the same as SlicingBlockDevice(bd, start, bd->size())
+     */
+    SlicingBlockDevice(BlockDevice *bd, bd_addr_t start);
+
+    /** Lifetime of the memory block device
+     *
+     *  @param bd       Block device to back the SlicingBlockDevice
+     *  @param start    Start block address to map to block 0, negative addresses
+     *                  are calculated from the end of the underlying block device
+     *  @param stop     End block address to mark the end of the block device,
+     *                  this block is not mapped, negative addresses are
+     *                  calculated from the end of the underlying block device
+     */
+    SlicingBlockDevice(BlockDevice *bd, bd_addr_t start, bd_addr_t end);
+
+    /** Lifetime of a block device
+     */
+    virtual ~SlicingBlockDevice() {};
+
+    /** Initialize a block device
+     *
+     *  @return         0 on success or a negative error code on failure
+     */
+    virtual bd_error_t init();
+
+    /** Deinitialize a block device
+     *
+     *  @return         0 on success or a negative error code on failure
+     */
+    virtual bd_error_t deinit();
+
+    /** Read blocks from a block device
+     *
+     *  @param buffer   Buffer to write blocks to
+     *  @param addr     Address of block to begin reading from
+     *  @param size     Size to read in bytes, must be a multiple of read block size
+     *  @return         0 on success, negative error code on failure
+     */
+    virtual bd_error_t read(void *buffer, bd_addr_t addr, bd_size_t size);
+
+    /** Program blocks to a block device
+     *
+     *  The blocks must have been erased prior to being programmed
+     *
+     *  @param buffer   Buffer of data to write to blocks
+     *  @param addr     Address of block to begin writing to
+     *  @param size     Size to write in bytes, must be a multiple of program block size
+     *  @return         0 on success, negative error code on failure
+     */
+    virtual bd_error_t program(const void *buffer, bd_addr_t addr, bd_size_t size) = 0;
+
+    /** Erase blocks on a block device
+     *
+     *  The state of an erased block is undefined until it has been programmed
+     *
+     *  @param addr     Address of block to begin erasing
+     *  @param size     Size to erase in bytes, must be a multiple of erase block size
+     *  @return         0 on success, negative error code on failure
+     */
+    virtual bd_error_t erase(bd_addr_t addr, bd_size_t size) = 0;
+
+    /** Sync an underlying block device
+     *
+     *  @return         0 on success or a negative error code on failure
+     */
+    virtual bd_error_t sync();
+
+    /** Get the size of a readable block
+     *
+     *  @return         Size of a readable block in bytes
+     */
+    virtual bd_size_t read_size();
+
+    /** Get the size of a writeable block
+     *
+     *  @return         Size of a writeable block in bytes
+     *  @note Must be a multiple of the read size, this is
+     *  equivalent to the erase size of the device
+     */
+    virtual bd_size_t write_size();
+
+    /** Get the size of a programable block
+     *
+     *  @return         Size of a programable block in bytes
+     *  @note Must be a multiple of the read size
+     */
+    virtual bd_size_t program_size();
+
+    /** Get the size of a eraseable block
+     *
+     *  @return         Size of a eraseable block in bytes
+     *  @note Must be a multiple of the program size
+     */
+    virtual bd_size_t erase_size();
+
+    /** Get the total size of the underlying device
+     *
+     *  @return         Size of the underlying device in bytes
+     */
+    virtual bd_size_t size();
+
+protected:
+    BlockDevice *_bd;
+    bool _start_from_end;
+    bd_size_t _start;
+    bool _stop_from_end;
+    bd_size_t _stop;
+};
+
+
+#endif

simonqhughes/mbed-os-1

features/filesystem/bd/ChainingBlockDevice.h
@@ -1,0 +1,153 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2017 ARM Limited
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#ifndef MBED_CHAINING_BLOCK_DEVICE_H
+#define MBED_CHAINING_BLOCK_DEVICE_H
+
+#include "BlockDevice.h"
+#include "mbed.h"
+
+
+/** Block device for chaining multiple block devices
+ *  with the similar block sizes at sequential addresses
+ *
+ *  @code
+ *  #include "mbed.h"
+ *  #include "HeapBlockDevice.h"
+ *  #include "ChainingBlockDevice.h"
+ *
+ *  // Create two smaller block devices with
+ *  // 64 and 32 blocks of size 512 bytes
+ *  HeapBlockDevice mem1(64*512, 512);
+ *  HeapBlockDevice mem2(32*512, 512);
+ *
+ *  // Create a block device backed by mem1 and mem2
+ *  // contains 96 blocks of size 512 bytes
+ *  BlockDevice *bds[] = {&mem1, &mem2};
+ *  ChainingBlockDevice chainmem(bds);
+ */
+class ChainingBlockDevice : public BlockDevice {
+public:
+    /** Lifetime of the memory block device
+     *
+     *  @param bds      Array of block devices to chain with sequential block addresses
+     *  @param count    Number of block devices to chain
+     *  @note All block devices must have the same block size
+     */
+    ChainingBlockDevice(BlockDevice **bds, size_t bd_count);
+
+    /** Lifetime of the memory block device
+     *
+     *  @param bds      Array of block devices to chain with sequential block addresses
+     *  @note All block devices must have the same block size
+     */
+    template <size_t Size>
+    ChainingBlockDevice(BlockDevice *(&bds)[Size])
+        : _bds(bds), _bd_count(sizeof(bds) / sizeof(bds[0]))
+        , _read_size(0), _program_size(0), _erase_size(0), _size(0) {
+    }
+
+    /** Lifetime of the memory block device
+     *
+     *  @param bds      Array of block devices to chain with sequential block addresses
+     *  @note All block devices must have the same block size
+     */
+    virtual ~ChainingBlockDevice() {}
+
+    /** Initialize a block device
+     *
+     *  @return         0 on success or a negative error code on failure
+     */
+    virtual bd_error_t init();
+
+    /** Deinitialize a block device
+     *
+     *  @return         0 on success or a negative error code on failure
+     */
+    virtual bd_error_t deinit();
+
+    /** Read blocks from a block device
+     *
+     *  @param buffer   Buffer to write blocks to
+     *  @param addr     Address of block to begin reading from
+     *  @param size     Size to read in bytes, must be a multiple of read block size
+     *  @return         0 on success, negative error code on failure
+     */
+    virtual bd_error_t read(void *buffer, bd_addr_t addr, bd_size_t size);
+
+    /** Program blocks to a block device
+     *
+     *  The blocks must have been erased prior to being programmed
+     *
+     *  @param buffer   Buffer of data to write to blocks
+     *  @param addr     Address of block to begin writing to
+     *  @param size     Size to write in bytes, must be a multiple of program block size
+     *  @return         0 on success, negative error code on failure
+     */
+    virtual bd_error_t program(const void *buffer, bd_addr_t addr, bd_size_t size);
+
+    /** Erase blocks on a block device
+     *
+     *  The state of an erased block is undefined until it has been programmed
+     *
+     *  @param addr     Address of block to begin erasing
+     *  @param size     Size to erase in bytes, must be a multiple of erase block size
+     *  @return         0 on success, negative error code on failure
+     */
+    virtual bd_error_t erase(bd_addr_t addr, bd_size_t size);
+
+    /** Get the size of a readable block
+     *
+     *  @return         Size of a readable block in bytes
+     */
+    virtual bd_size_t get_read_size();
+
+    /** Get the size of a programable block
+     *
+     *  @return         Size of a programable block in bytes
+     *  @note Must be a multiple of the read size
+     */
+    virtual bd_size_t get_program_size();
+
+    /** Get the size of a eraseable block
+     *
+     *  @return         Size of a eraseable block in bytes
+     *  @note Must be a multiple of the program size
+     */
+    virtual bd_size_t get_erase_size();
+
+    /** Get the total size of the underlying device
+     *
+     *  @return         Size of the underlying device in bytes
+     */
+    virtual bd_size_t size();
+
+protected:
+    BlockDevice **_bds;
+    size_t _bd_count;
+    bd_size_t _read_size;
+    bd_size_t _program_size;
+    bd_size_t _erase_size;
+    bd_size_t _size;
+};
+
+
+#endif

features/filesystem/bd/SlicingBlockDevice.h
@@ -1,0 +1,151 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2017 ARM Limited
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#ifndef MBED_SLICING_BLOCK_DEVICE_H
+#define MBED_SLICING_BLOCK_DEVICE_H
+
+#include "BlockDevice.h"
+#include "mbed.h"
+
+
+/** Block device for mapping to a slice of another block device
+ *
+ *  @code
+ *  #include "mbed.h"
+ *  #include "HeapBlockDevice.h"
+ *  #include "SlicingBlockDevice.h"
+ *
+ *  // Create a block device with 64 blocks of size 512
+ *  HeapBlockDevice mem(64*512, 512);
+ *
+ *  // Create a block device that maps to the first 32 blocks
+ *  SlicingBlockDevice slice1(&mem, 0*512, 32*512);
+ *
+ *  // Create a block device that maps to the last 32 blocks
+ *  SlicingBlockDevice slice2(&mem, 32*512);
+ *
+ *  // Create a block device that maps to the middle 32 blocks
+ *  SlicingBlockDevice slice3(&mem, 16*512, -16*512);
+ */
+class SlicingBlockDevice : public BlockDevice {
+public:
+    /** Lifetime of the memory block device
+     *
+     *  @param bd       Block device to back the SlicingBlockDevice
+     *  @param start    Start block address to map to block 0, negative addresses
+     *                  are calculated from the end of the underlying block device
+     *  @note This is the same as SlicingBlockDevice(bd, start, bd->size())
+     */
+    SlicingBlockDevice(BlockDevice *bd, bd_addr_t start);
+
+    /** Lifetime of the memory block device
+     *
+     *  @param bd       Block device to back the SlicingBlockDevice
+     *  @param start    Start block address to map to block 0, negative addresses
+     *                  are calculated from the end of the underlying block device
+     *  @param stop     End block address to mark the end of the block device,
+     *                  this block is not mapped, negative addresses are
+     *                  calculated from the end of the underlying block device
+     */
+    SlicingBlockDevice(BlockDevice *bd, bd_addr_t start, bd_addr_t end);
+
+    /** Lifetime of a block device
+     */
+    virtual ~SlicingBlockDevice() {};
+
+    /** Initialize a block device
+     *
+     *  @return         0 on success or a negative error code on failure
+     */
+    virtual bd_error_t init();
+
+    /** Deinitialize a block device
+     *
+     *  @return         0 on success or a negative error code on failure
+     */
+    virtual bd_error_t deinit();
+
+    /** Read blocks from a block device
+     *
+     *  @param buffer   Buffer to write blocks to
+     *  @param addr     Address of block to begin reading from
+     *  @param size     Size to read in bytes, must be a multiple of read block size
+     *  @return         0 on success, negative error code on failure
+     */
+    virtual bd_error_t read(void *buffer, bd_addr_t addr, bd_size_t size);
+
+    /** Program blocks to a block device
+     *
+     *  The blocks must have been erased prior to being programmed
+     *
+     *  @param buffer   Buffer of data to write to blocks
+     *  @param addr     Address of block to begin writing to
+     *  @param size     Size to write in bytes, must be a multiple of program block size
+     *  @return         0 on success, negative error code on failure
+     */
+    virtual bd_error_t program(const void *buffer, bd_addr_t addr, bd_size_t size);
+
+    /** Erase blocks on a block device
+     *
+     *  The state of an erased block is undefined until it has been programmed
+     *
+     *  @param addr     Address of block to begin erasing
+     *  @param size     Size to erase in bytes, must be a multiple of erase block size
+     *  @return         0 on success, negative error code on failure
+     */
+    virtual bd_error_t erase(bd_addr_t addr, bd_size_t size);
+
+    /** Get the size of a readable block
+     *
+     *  @return         Size of a readable block in bytes
+     */
+    virtual bd_size_t get_read_size();
+
+    /** Get the size of a programable block
+     *
+     *  @return         Size of a programable block in bytes
+     *  @note Must be a multiple of the read size
+     */
+    virtual bd_size_t get_program_size();
+
+    /** Get the size of a eraseable block
+     *
+     *  @return         Size of a eraseable block in bytes
+     *  @note Must be a multiple of the program size
+     */
+    virtual bd_size_t get_erase_size();
+
+    /** Get the total size of the underlying device
+     *
+     *  @return         Size of the underlying device in bytes
+     */
+    virtual bd_size_t size();
+
+protected:
+    BlockDevice *_bd;
+    bool _start_from_end;
+    bd_size_t _start;
+    bool _stop_from_end;
+    bd_size_t _stop;
+};
+
+
+#endif